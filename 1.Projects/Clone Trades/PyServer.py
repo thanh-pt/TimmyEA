f7c030cbd16336e78cc11af1d8d56cc1ce540b49
ThanhPham <tienthanh.pham@hotmail.com>
2024-11-20 11:17:00 +0700

    add PyServer và đổi tên thư mục

---
+++ 1.Projects/Clone Trades/PyServer.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+shared_msg = "---"
+
+# API thêm giao dịch
+@app.route('/api/send', methods=['POST'])
+def send_trade():
+    global shared_msg
+    shared_msg = request.data.decode('utf-8');
+    return "OK", 200
+
+# API lấy toàn bộ danh sách giao dịch
+@app.route('/api/get', methods=['GET'])
+def get_trades():
+    return shared_msg, 200
+
+# Chạy server
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
